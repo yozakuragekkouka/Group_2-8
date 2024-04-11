e640d8375bed4825a7efee0d1e33317478e0c58b
yozakuragekkouka <tsukitohikaritoyami@icloud.com>
2024-04-11 10:10:25 +0900

    gazou

--- Src/Nishiyama/Scene/SceneTitle.h
+++ Src/Nishiyama/Scene/SceneTitle.h
@@ -4,15 +4,14 @@
 #include "../Sound/Sound.h"
 
 constexpr char TITLE_BG_PATH[128] = "Data/Image/SceneTitle/TitleBackGround.png";
-constexpr char TITLE_SOLO_TEXT_PATH[128] = "Data/Image/Title/Solo.png";
-constexpr char TITLE_MULTI_TEXT_PATH[128] = "Data/Image/Title/Multi.png";
+constexpr char TITLE_SOLO_TEXT_PATH[128] = "Data/Image/SceneTitle/TitleBottan.png";
+constexpr char TITLE_MULTI_TEXT_PATH[128] = "Data/Image/SceneTitle/TitleBottan.png";
 constexpr char TITLE_MULTI_CARD_PATH[4][128] =
 {
-	"Data/Image/Card/1.png",
-	"Data/Image/Card/2.png",
-	"Data/Image/Card/3.png",
-	"Data/Image/Card/4.png",
-	
+	"Data/Image/ScenePlay/トランプスペード_1.png",
+	"Data/Image/ScenePlay/トランプスペード_2.png",
+	"Data/Image/ScenePlay/トランプスペード_3.png",
+	"Data/Image/ScenePlay/トランプスペード_4.png"
 };
 
 
