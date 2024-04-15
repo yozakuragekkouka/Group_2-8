ae91912fef40e0a3ce57be4d8869c413a42cb09a
ster1271 <144640199+ster1271@users.noreply.github.com>
2024-04-15 23:01:22 +0900

    画像表示

--- Src/Nishiyama/Scene/SceneResult.cpp
+++ Src/Nishiyama/Scene/SceneResult.cpp
@@ -6,12 +6,21 @@
 #include "Scene.h"
 #include "SceneResult.h"
 #include "../NumberFont/NumberFont.h"
+#include "../Score/ScoreManager/ScoreManager.h"
 
 //クリア初期化
 void SceneResult::Init()
 {
 	Result_BG_Hndl = LoadGraph(RESULT_BG_PATH);
 	TextHndl = LoadGraph(TEXT_PATH);
+
+	ClownHndl = LoadGraph("Data/Image/SceneResult/王冠.png");
+	PlayerHndl[0] = LoadGraph("Data/Image/ScenePlay/プレイヤー1.png");
+	PlayerHndl[1] = LoadGraph("Data/Image/ScenePlay/プレイヤー2.png");
+	PlayerHndl[2] = LoadGraph("Data/Image/ScenePlay/プレイヤー3.png");
+	PlayerHndl[3] = LoadGraph("Data/Image/ScenePlay/プレイヤー4.png");
+	PlayerHndl[4] = LoadGraph("Data/Image/ScenePlay/CPU_400.png");
+
 
 	BGM_Hndl = LoadSoundMem(RESULT_BMG_PATH);			//BGM読み込み
 	TAPSound_Hndl = LoadSoundMem(RESULT_TAP_PATH);		//決定音読み込み
@@ -21,6 +30,9 @@ void SceneResult::Init()
 	MousePosX = MousePosY = 0;
 
 	PlaySoundMem(BGM_Hndl, DX_PLAYTYPE_LOOP);
+
+	//テスト用
+	//ScoreManager::SetWinner(1);
 
 	SceneManager::g_CurrenySceneID = SCENEID::SCENE_ID_LOOP_RESULT;
 }
@@ -41,16 +53,22 @@ void SceneResult::Step()
 			SceneManager::g_CurrenySceneID = SCENEID::SCENE_ID_FIN_RESULT;
 		}
 	}
+
+	
 }
 
 //クリア描画処理
 void SceneResult::Draw()
 {
+
 	DrawGraph(0, 0, Result_BG_Hndl, true);				//背景画像描画
 	DrawGraph(TextPosX, TextPosY, TextHndl, true);		//テキスト画像描画
 
-	/*DrawFormatString(0, 0, GetColor(255, 255, 255), "マウスのX座標：%d", MousePosX);
-	DrawFormatString(0, 15, GetColor(255, 255, 255), "マウスのY座標：%d", MousePosY);*/
+	DrawRotaGraph(640, 320, 0.7f, 0.0f, PlayerHndl[ScoreManager::GetWinner()], true, false, false);	//勝者プレイヤーの描画
+	DrawRotaGraph(640, 120, 0.7f, 0.0f, ClownHndl, true, false, false);		//王冠画像表示
+
+	DrawFormatString(0, 0, GetColor(255, 255, 255), "マウスのX座標：%d", MousePosX);
+	DrawFormatString(0, 15, GetColor(255, 255, 255), "マウスのY座標：%d", MousePosY);
 
 }
 
@@ -59,6 +77,13 @@ void SceneResult::Fin()
 {
 	DeleteGraph(Result_BG_Hndl);
 	DeleteGraph(TextHndl);
+	DeleteGraph(ClownHndl);
+
+	for (int i = 0; i < 5; i++)
+	{
+		DeleteGraph(PlayerHndl[i]);
+	}
+
 
 	DeleteSoundMem(BGM_Hndl);
 	DeleteSoundMem(TAPSound_Hndl);
--- Src/Nishiyama/Scene/SceneResult.h
+++ Src/Nishiyama/Scene/SceneResult.h
@@ -7,9 +7,10 @@
 #define RESULT_BMG_PATH		"Data/Sound/SceneResult/SceneResult.mp3"
 #define RESULT_TAP_PATH		"Data/Sound/SceneResult/決定ボタンを押す47.mp3"
 
-
 #define TEXT_WIDHT			(500)	//テキストの横幅
 #define TEXT_HEIGHT			(200)	//テキストの縦幅
+
+
 
 class SceneResult : public SceneBase
 {
@@ -17,6 +18,9 @@ private:
 	int Result_BG_Hndl;		//背景画像格納変数
 	int TextHndl;			//テキスト画像格納変数
 	int TextPosX, TextPosY;	//テキストの座標変数
+
+	int ClownHndl;			//冠画像変数
+	int PlayerHndl[5];		//プレイヤー画像格納変数
 
 	int BGM_Hndl;			//BGM格納変数
 	int TAPSound_Hndl;		//決定音格納変数
